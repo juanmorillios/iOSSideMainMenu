de806cd169cb6fd9b8bb7836a59929d72c3b5f7d
Juan Morillo <juanmorillios@gmail.com>
2017-05-20 23:32:57 +0200

    Create project iOSSideMainMenu

---
+++ Example/Pods/Target Support Files/Pods-iOSSideMainMenu_Example/Pods-iOSSideMainMenu_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSSideMainMenu_Example : NSObject
+@end
+@implementation PodsDummy_Pods_iOSSideMainMenu_Example
+@end
---
+++ Example/Pods/Target Support Files/iOSSideMainMenu/iOSSideMainMenu-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_iOSSideMainMenu : NSObject
+@end
+@implementation PodsDummy_iOSSideMainMenu
+@end
